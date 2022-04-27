b6b9e7f523367b1e134d9ca4f694ad4b99227c35
Jared Ruiz <jared707@gmail.com>
2022-04-27 17:27:58 -0500

    Added a db query to add a new department to database.

--- index.js
+++ index.js
@@ -17,17 +17,43 @@ const beginPrompt = () => {
     ])
     //take inquirer data and parse for selection
     .then(selectionData => {
+        //view all departments
         if (selectionData.startSelection === 'View All Departments') {
             console.log('View All Departments Was Selected');
             getAllDepartments();
         }
+        //view all roles
         else if (selectionData.startSelection === 'View All Roles') {
             console.log('View All Roles Was Selected');
             getAllRoles();
         }
+        //view all employees
         else if (selectionData.startSelection === 'View All Employees') {
             console.log('View All Employees Was Selected');
             getAllEmployees();
+        }
+        //add a department
+        else if (selectionData.startSelection === 'Add a Department') {
+            console.log('Add a Department Was Selected');
+            addDepartment();
+        }
+        //add a role
+        else if (selectionData.startSelection === 'Add a Role') {
+            console.log('Add a Role Was Selected');
+            addRole();
+        }
+        //add an employee
+        else if (selectionData.startSelection === 'Add an Employee') {
+            console.log('Add an Employee Was Selected');
+            addEmployee();
+        }
+        //update an existing employee
+        else if (selectionData.startSelection === 'Update an Employee') {
+            console.log('Update an Employee Was Selected');
+            updateEmployee();
+        }
+        else {
+            return;
         }
     });
 }
@@ -90,6 +116,33 @@ getAllDepartments = (selectionData) => {
         //confirm return to starting selection
         returnToPrompt();
     });
+}
+
+addDepartment = () => {
+
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'departmentName',
+            message: 'Please provide the name of the department you would like to add.'
+        }
+    ])
+    .then(departmentInfo => {
+        const sql = `INSERT INTO department (name)
+        VALUES (?)`;
+        const params = departmentInfo.departmentName;
+
+        db.query(sql, params, (err, results) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log(`Successfully added ${departmentInfo.departmentName} to the database.`);
+            
+            //ask if they would like to return to database selection
+            returnToPrompt();
+        });
+        
+    })
 }
 
 //connect to database upon run, else return err
